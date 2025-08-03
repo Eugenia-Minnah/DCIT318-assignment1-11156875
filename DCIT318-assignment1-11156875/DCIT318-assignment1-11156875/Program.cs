4c624096d1e9c9549785a18aba52be919e30e325
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2025-08-03 12:27:45 +0000

    Added main program entry point with application selector

--- DCIT318-assignment1-11156875/DCIT318-assignment1-11156875/Program.cs
+++ DCIT318-assignment1-11156875/DCIT318-assignment1-11156875/Program.cs
@@ -1,1 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DCIT318_assignment1_11156875;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Select an application to run:");
+        Console.WriteLine("1. Grade Calculator");
+        Console.WriteLine("2. Ticket Price Calculator");
+        Console.WriteLine("3. Triangle Type Identifier");
+
+        Console.Write("Enter your choice (1-3): ");
+        string choice = Console.ReadLine();
+
+    }
+}
